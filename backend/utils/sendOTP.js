7e8e5e3f018d635a8d0c00e86b010d19367ee7af
spriveen <rpeanuka@gmail.com>
2026-06-26 14:17:47 +0530

    update sendOtp.js

---
+++ backend/utils/sendOTP.js
@@ -0,0 +1,19 @@
+import { createTransport } from "nodemailer";
+
+const sendOtp = async (ElementInternals, otp)=>{
+    const transporter = createTransport({
+        service: "gmail",
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS
+        }
+    });
+    await transporter.sendMail({
+        from: process.env.EMAIL_USER,
+        to: email,
+        subject: "Your OTP Code",
+        html: `<h2> Your OTP is ${otp}</h2>`
+    })
+}
+
+export default sendOtp
