ff789865c970396af3654a841ed32bad77ee9f43
spriveen <rpeanuka@gmail.com>
2026-06-27 10:53:57 +0530

    update step authcontroller.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,5 +1,8 @@
 import { generate } from "otp-generator";
 import User from "../models/User.js";
+import sendOtp from "../utils/sendOTP.js";
+import bcrypt from 'bcryptjs';
+import{v4 as uuidv4} from 'uuid';
 
 
 
@@ -32,14 +35,59 @@ export async function registerUser(req,res){
 
 //   to send otp 
 try {
-    
-} catch (error) {
-    
-} 
+    await sendOtp(email, otp);
 
+} catch (emailError) {
+   console.error("Error sending OTP email:", emailError);
+   return res.status(500).json({
+    message: "Failed to send OTP email.Please try again."
+   });
+} 
+   const hashedpassword = await bcrypt.hash(password,10);
+   const otpExpiry = new Date(Date.now()+ 5*60*1000);
+   const studentId = `ST-${uuidv4().slice(0,8).toUpperCase()}`
+
+   const user = await User.create({
+    name, email, phone: cleanPhone,
+    password: hashedpassword,
+    otp,
+    otpExpiry,
+    studentId
+   });
+   res.status(201).json({
+    message: "User registered sucessgully, OTP sent to email", user
+   });
     }
     
     catch (error) {
-        
+      console.error("Error registering user:", error);
+        res.status(500).json({message: "Error registering user", error:error.message})
+    }
+}
+
+// Step 2: verify the otp
+export async function verifyOtp(req,res){
+    try {
+      const {email, otp} = req.body;
+      if(!email) return res.status(400).json({ message: "Email is required"});
+
+      const user = await User.findOne({email});
+      if(!user) return res.status(400).json({message: "User not found"})
+       if (user.otp !== otp || new Date()> new Date(user.otpExpiry)) {
+        return res.status(400).json({
+       message: "Invalid or expired OTP"
+    });
+   }  
+   Object.assign(user, {isVerified: true, otp: null, otpExpiry: null});
+   await user.save();
+   res.status(200).json({message: "OTP verified sucessfully"});
+    
+    } 
+    
+    catch (error) {
+       console.error("Error verifying OTP:", error) ;
+       res.status(500).json({
+        message: "Error verifying OTP", error: error.message
+       });
     }
 }
