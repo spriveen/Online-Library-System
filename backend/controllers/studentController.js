aa19263d1ea68f41f2838613ec0760db53f04d90
spriveen <rpeanuka@gmail.com>
2026-06-28 09:09:30 +0530

    update studentcontroller.js

---
+++ backend/controllers/studentController.js
@@ -0,0 +1,45 @@
+import  User from "../models/User.js";
+
+//  to search the student by roll no
+export async function searchStudentsByRoll(req,res){
+    try {
+      const roll = String(req.query.roll || "").trim();
+      if(!roll)  {
+        return res.status(200).json({ sucess:true, students:[]});
+
+      }
+
+      const rollRegex = new RegExp(roll, "i");
+    const students = await User.find({
+        role: "user",
+        isProfileComplete: true,
+        rollNo: {$regex: rollRegex}
+    })
+    .select("name email department stram semester year rollNo")
+    .limit(12);
+
+    const mappedStudents = students.map((student) => ({
+  name: student.name,
+  email: student.email,
+  department: student.department || "",
+  stream: student.stream || "",
+  academicYear: student.year || "",
+  semester: student.semester || "",
+  rollNumber: student.rollNo || "",
+}));
+
+res.status(200).json({
+    sucess: true,
+    students: mappedStudents
+})
+    
+    } 
+    
+    catch (error) {
+        console.error("Error searching students by roll:",  error);
+        res.status(500).json({
+            sucess: false,
+            message: "Error searchinng students by roll"
+        });
+    }
+}
