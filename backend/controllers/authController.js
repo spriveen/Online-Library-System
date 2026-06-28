222f582da650778573220b789ddab9c1fa0027e6
spriveen <rpeanuka@gmail.com>
2026-06-28 07:22:51 +0530

    sucessfully auth controller.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -240,3 +240,44 @@ export async function getUsers(req,res){
         res.status(500).json({message: "Error fetching students", error: errror.message})
     }
 }
+
+// for admin registration
+export async function registerAdmin(req,res){
+    try {
+       const {name, email, phone,password}= req.body;
+        if(!name || !email || !phone || !password){
+           return res.status(400).json({
+            message: "Please enter all required fields"
+           });
+        }
+        if(await User.findOne({email})) {
+           return res.status(400).json({
+            message: "User already exists with this email"
+           }) ;
+        }
+
+        const hashedpassword = await bcrypt.hash(password,10);
+        const user = await User.create({
+            name,
+            email: email.trim().toLowerCase(),
+            phone,
+            password: hashedpassword,
+            role: "admin",
+            isVerified: true
+        });
+         const {password:_, ...userResponse}= user.toObject();
+         res.status(201).json({
+            sucess:true,
+            message: "Admin registerd sucessfully",
+            user: userResponse
+
+         })
+
+
+    } 
+    
+    catch (error) {
+        console.error("Error registering admin:", error);
+        res.status(500).json({message: "Error registering admin", error: errror.message})
+    }
+}
