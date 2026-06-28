6d2bf5099c129605156422d1366422fe44c24298
spriveen <rpeanuka@gmail.com>
2026-06-28 07:41:22 +0530

    update authMiddleware.js

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,35 @@
+import User from "../models/User.js";
+import jwt from 'jsonwebtoken';
+
+// to athenticate JWT Token
+export const authenticateToken = async (req,res, next) =>{
+    try {
+     const authHeader = req.headers["authorization"] ;
+     const token = authHeader && authHeader.split("")[1];
+     
+     if(!token) {
+       return res.status(401).json({
+        message: "No tokem provided, authorization denied"
+       }) ;
+     }
+
+     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+     const user = await User.findById(decoded.id).select("-password");
+
+     if(!user) {
+        return res.status(401).json({
+        message: "Token is not valid or user no longer exists"
+        });
+     }
+     req.user = user;
+     next();
+
+    } 
+    
+    catch (error) {
+      console.error("JWT Auth error:", error);
+      return res.status(401).json({
+        message: "Token is not valid"
+      });
+    }
+}
